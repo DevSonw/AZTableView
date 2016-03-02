2a1766864bdc83f1d42fd463f74c1be22337fb11
Arron <arronzhang@me.com>
2016-03-02 16:13:49 +0800

    Init table

---
+++ AZTableView/AZTableView.h
@@ -0,0 +1,13 @@
+//
+//  AZTableView.h
+//  AZTableViewExample
+//
+//  Created by Arron Zhang on 16/3/2.
+//  Copyright © 2016年 Arron Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AZTableView : UITableView
+
+@end
